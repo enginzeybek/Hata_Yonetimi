e0d5c45257f5e7172b3fb8103587f80709b792a5
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2024-10-12 21:32:42 +0300

    Hata yonetimi

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+try
+{
+    Console.WriteLine("Bir sayı giriniz");
+    int sayi = int.Parse(Console.ReadLine());
+    Console.WriteLine("Girilen Sayi: " + sayi);
+}
+catch(Exception ex) 
+{
+    Console.WriteLine("Hata: " + ex.Message.ToString());
+}
+finally
+{
+    Console.WriteLine("İşlem tamamlandı"); // hata alsın almasın kullanılan bulok finally.
+}
+
+
